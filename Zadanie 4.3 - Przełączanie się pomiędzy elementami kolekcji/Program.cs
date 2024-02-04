922ebb219bf84507e8cce77bbc11b243ffa22b78
Romeq03 <romekbartek18@gmail.com>
2024-02-04 21:36:46 +0100

    first commit

---
+++ Zadanie 4.3 - Przełączanie się pomiędzy elementami kolekcji/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] tablica = new int[100];
+
+        for (int i = 0; i < 100; i++)
+        {
+            tablica[i] = i + 1;
+        }
+
+        int sumaParzyste = 0;
+        int sumaNieparzyste = 0;
+
+        foreach (int liczba in tablica)
+        {
+            if (liczba % 2 == 0)
+            {
+                sumaParzyste += liczba;
+            }
+            else
+            {
+                sumaNieparzyste += liczba;
+            }
+        }
+
+        Console.WriteLine("Suma liczb parzystych: " + sumaParzyste);
+        Console.WriteLine("Suma liczb nieparzystych: " + sumaNieparzyste);
+
+        Console.ReadLine();
+    }
+}
